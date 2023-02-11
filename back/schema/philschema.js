eead9e3bbfab209410204f6d6605a8982d7e9b4e
sahana-27 <86240048+sahana-27@users.noreply.github.com>
2023-02-11 22:23:03 +0530

    Update philschema.js

--- back/schema/philschema.js
+++ back/schema/philschema.js
@@ -1,11 +1,27 @@
 var mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 var philanthropistSchema = new mongoose.Schema({
     name: String,
+    email: String,
     password: String,
     location: String,
-    donation_preferences: [String],
-    past_donations: [String]
+    NGOPref: [String],
+    pastDonations: String
 });
+
+//password encryption
+philanthropistSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+})
+
+//checking if password matches
+philanthropistSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
 
 var Philanthropist = mongoose.model('Phil', philanthropistSchema);
 
